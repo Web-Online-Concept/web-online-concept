3a88ee14719d9d1d8a43b064b897045c7642a0c6
Web-Online-Concept <web.online.concept@gmail.com>
2025-07-30 17:11:16 +0200

    Update page.js

--- app/admin/facturation/nouveau/page.js
+++ app/admin/facturation/nouveau/page.js
@@ -1,7 +1,18 @@
 "use client"
 
+import { Suspense } from 'react'
 import EditDocument from '../[docId]/page'
 
-export default function NewDocument() {
+export const dynamic = 'force-dynamic'
+
+function DocumentContent() {
   return <EditDocument />
+}
+
+export default function NewDocument() {
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <DocumentContent />
+    </Suspense>
+  )
 }
