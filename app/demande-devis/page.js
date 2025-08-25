f33bf2d0fccb0570a6d332deccc1deb41e40548c
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-26 00:24:09 +0200

    Update page.js

--- app/demande-devis/page.js
+++ app/demande-devis/page.js
@@ -276,8 +276,7 @@ export default function DemandeDevis() {
                 <input
                   type="text"
                   required
-                  value={nomComplet}
-                  onChange={(e) => setNomComplet(e.target.value)}
+                  defaultValue=""
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0073a8]"
                 />
               </div>
