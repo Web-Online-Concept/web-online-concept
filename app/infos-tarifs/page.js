027c3ba998e90e8ba397ae56ea828eab4da6fd1e
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-25 09:26:49 +0200

    Create page.js

---
+++ app/infos-tarifs/page.js
@@ -0,0 +1,8 @@
+export default function InfosTarifs() {
+  return (
+    <div style={{paddingTop: '100px', textAlign: 'center'}}>
+      <h1 style={{fontSize: '48px', color: 'red'}}>TEST - LA PAGE FONCTIONNE</h1>
+      <p style={{fontSize: '24px'}}>Si vous voyez ceci, le déploiement marche</p>
+    </div>
+  )
+}
