e20f503a1a4864fb4425931440e45635fa1ec386
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-26 10:06:21 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -99,6 +99,12 @@ export default function Header() {
                 className="absolute bottom-0 left-0 w-0 h-[2px] bg-gradient-to-r from-[#0073a8] to-[#00b4d8] transition-all duration-300 group-hover:w-full"
               />
             </Link>
+            <Link href="/realisations" className="relative text-gray-700 hover:text-[#0073a8] transition-colors group">
+              Réalisations
+              <span 
+                className="absolute bottom-0 left-0 w-0 h-[2px] bg-gradient-to-r from-[#0073a8] to-[#00b4d8] transition-all duration-300 group-hover:w-full"
+              />
+            </Link>
             <Link href="/infos-tarifs" className="relative text-gray-700 hover:text-[#0073a8] transition-colors group">
               Infos & Tarifs
               <span 
@@ -134,6 +140,13 @@ export default function Header() {
               onClick={() => setIsMenuOpen(false)}
             >
               Services
+            </Link>
+            <Link 
+              href="/realisations" 
+              className="block py-2 text-gray-700 hover:text-[#0073a8]"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Réalisations
             </Link>
             <Link 
               href="/infos-tarifs" 
