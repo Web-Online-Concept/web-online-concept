13390879e6c45c7535249730b2bc2f3d190246a9
Web-Online-Concept <web.online.concept@gmail.com>
2025-09-03 14:36:42 +0200

    Update page.js

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -258,7 +258,7 @@ export default async function ArticlePage({ params }) {
                 </svg>
                 LinkedIn
               </a>
-              <button
+              {/* <button
                 onClick={() => navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_BASE_URL}/blog/${article.slug}`)}
                 className="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg hover:bg-gray-300 transition-colors flex items-center gap-2"
               >
@@ -266,7 +266,7 @@ export default async function ArticlePage({ params }) {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
                 </svg>
                 Copier le lien
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
