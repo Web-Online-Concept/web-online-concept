936c50404758450860433c16f0990ca86229c6ff
Web-Online-Concept <web.online.concept@gmail.com>
2025-09-09 20:06:17 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import '../styles/quill-custom.css'
 import Header from '@/components/Header'
@@ -67,6 +68,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FPQGE1LFH1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FPQGE1LFH1');
+          `}
+        </Script>
+
         <Header />
         <main>{children}</main>
         <Footer />
